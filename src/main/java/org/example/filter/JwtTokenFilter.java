87ead78905899a9a1f98b4633268f68d56c3ccb6
mihaillepenkin <mihaillepenkin@gmail.com>
2025-03-02 14:17:49 +0300

    qwe

---
+++ src/main/java/org/example/filter/JwtTokenFilter.java
@@ -0,0 +1,27 @@
+package org.example.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@Slf4j
+public class JwtTokenFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            log.info(token);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
